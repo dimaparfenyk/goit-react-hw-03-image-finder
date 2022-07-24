3901f0a08fb2cc3d346c72b6461c5e60f5735e7a
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-07-25 01:12:08 +0300

    update logic of app and form

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,22 +1,59 @@
+import { Component } from "react";
 import { FiSearch } from "react-icons/fi";
+import {toast } from 'react-toastify';
+
+
 import { Searchbar, SearchForm, SearchFormBtn,SearchFormBtnLabel, SearchFormInput } from "./SearchBar.styled";
 
-export const SearchBar = (onSubmit) => {
-    return (
-        <Searchbar className="Searchbar">
-            <SearchForm className="form">
-                <SearchFormBtn type="submit" className="button">
-                    <SearchFormBtnLabel className="button-label"><FiSearch/></SearchFormBtnLabel>
-                </SearchFormBtn>
+export class SearchBar extends Component {
+    
+    state = {
+        itemName: '',
+    };
+    
+    handleNameChange = e => {
+        this.setState({ itemName: e.currentTarget.value.toLowerCase() });
 
-                <SearchFormInput
-                    className="input"
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images"
-                />
-            </SearchForm>
-        </Searchbar>
-    )
+    };
+
+   handleSubmit = e => {
+       e.preventDefault();
+       
+       if (this.state.itemName.trim()==='') {
+        toast.error('Введите корректное значение!', {
+            position: "top-right",
+            autoClose: 3000,
+            });
+        return
+       };
+
+       this.props.onSubmit(this.state.itemName);
+       this.setState({itemName:''})
+    };
+    render() {
+        return (
+            <Searchbar className="Searchbar">
+              
+                <SearchForm
+                    className="form"
+                    onSubmit={this.handleSubmit}
+                >
+                    <SearchFormBtn type="submit" className="button">
+                        <FiSearch></FiSearch>
+                        <SearchFormBtnLabel className="button-label">Search</SearchFormBtnLabel>
+                    </SearchFormBtn>
+
+                    <SearchFormInput
+                        className="input"
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images"
+                        value={this.state.itemName}
+                        onChange={this.handleNameChange}
+                    />
+                </SearchForm>
+            </Searchbar>
+        )
+    }
 };
